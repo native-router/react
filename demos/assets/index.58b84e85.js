51e027c867235bb93744a4c4a388aba7cd7b42fe
wmzy <1256573276@qq.com>
2022-12-17 15:54:44 +0800

    Updates

---
+++ demos/assets/index.58b84e85.js
@@ -0,0 +1,19 @@
+import { j as jsxs, a as jsx } from './index.3fbfe50f.js';
+
+var index_53jxoq = '';
+
+/**
+ * About 页面
+ */
+function About() {
+  return /*#__PURE__*/jsxs("div", {
+    className: "dmoxuxe",
+    children: [/*#__PURE__*/jsx("h1", {
+      children: "About Native Router"
+    }), /*#__PURE__*/jsx("p", {
+      children: "Native Router is another router lib which work like the native browser."
+    })]
+  });
+}
+
+export { About as default };
